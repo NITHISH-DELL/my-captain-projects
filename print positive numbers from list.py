17fde41df84935ff5037014fa6d1caacc1ec14c9
NITHISH-DELL <67948580+NITHISH-DELL@users.noreply.github.com>
2020-08-03 09:29:25 +0000

    Add files via upload

---
+++ print positive numbers from list.py
@@ -0,0 +1,14 @@
+#### printing the positive numbers #####
+
+##initialization##
+list1=[12,-7,5,64,-14]
+for i in list1:
+     if i>=0: # checking positive number
+          print(i)# printing the positive number
+
+print("\n")
+## initialization ##
+list2=[12,14,-95,3]
+for j in list2:
+     if j>=0:# checking positive number
+          print(j)# printing the positive number
